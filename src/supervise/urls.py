374630823b3ab4b2aee18c5e255a4007ab34f4fc
Oscar Carballal Prego <info@oscarcp.com>
2011-05-27 21:13:55 +0200

    Added main urls.py file

--- src/supervise/urls.py
+++ src/supervise/urls.py
@@ -1,17 +1,41 @@
-from django.conf.urls.defaults import patterns, include, url
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    supervise.urls
+    ~~~~~~~~~~~~~~
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+    This file distributes all the URLs around Supervise apps.
+
+    :copyright: (c) 2011 by Oscar Carballal Prego <oscarcp@clionesoftware.com>
+    :license: Simplified BSD License, see LICENSE for more details.
+"""
+
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'supervise.views.home', name='home'),
-    # url(r'^supervise/', include('supervise.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Django administration interface
+    url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # Static content #### FOR DEVELOPMENT!! ####
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': 'static'}),
+
+    # Uploads content #### FOR DEVELOPMENT!! ####
+    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': 'uploads'}),
+
+
+    # Supervise index
+    url(r'^$', 'supervise.views.home', name='home'),
+    
+    # i18n language switcher
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
+    # Project index    
+    url(r'^(?P<project_name>[\w\-]+)/', include('supervise.apps.projects.urls'),
+        name='projects'),
 )
