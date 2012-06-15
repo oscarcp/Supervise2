12642e6eab03f830994e62b6839915eb47f60e19
Oscar Carballal Prego <info@oscarcp.com>
2012-06-15 19:40:08 +0200

    Added userena and admin urls

--- src/supervise/urls.py
+++ src/supervise/urls.py
@@ -1,17 +1,37 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 Clione Software
+# Authors: Oscar Carballal Prego <oscar@clionesoftware.com>
+# License: BSD Simplified (2-Clause BSD). See LICENSE for details.
+#
+# This file is part of Supervise project.
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.utils.translation import ugettext_lazy as _
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+
     # url(r'^$', 'supervise.views.home', name='home'),
+
     # url(r'^supervise/', include('supervise.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # User accounts URLs
+    url(r'^u/', include('apps.thirdparty.userena.urls')),
+
 )
+
+if settings.DEBUG:
+    # Serve static files
+    urlpatterns += staticfiles_urlpatterns()
+    # Serve uploaded files
+    urlpatterns += patterns('',
+        url(r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': settings.MEDIA_ROOT}),
+    )
